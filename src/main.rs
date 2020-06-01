809d9aa3718ed2e98f26c5dce0dfce5e483c5dc1
Lionel Beato <beato@lionel.works>
2020-06-01 19:14:23 -0400

    adds comments to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// initialized functions are created using the keyword "fn"
+// these are the building blocks of any and all code
+// this main() function is an entry point and serves as the way we run our program
 fn main() {
+    // this is print function is a macro
+    // macros are easily accessed pieces of code that are denoted with the exclamation point
     println!("Hello, world!");
 }

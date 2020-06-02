20cc96ef56cb143ccd7d47c7f75cba83039cb7f6
Lionel Beato <beato@lionel.works>
2020-06-01 20:41:03 -0400

    adds comments section

---
+++ src/comments.rs
@@ -0,0 +1,17 @@
+// this is a line comment!
+// comments are ignored by the compiler and will never be treated as code
+
+/*
+*
+*
+*  You can create block comments as well using the asterisk symbol. 
+*
+*
+*
+*
+*
+*/
+
+pub fn comment_on_comments() {
+    println!("comments are cool!");
+}
